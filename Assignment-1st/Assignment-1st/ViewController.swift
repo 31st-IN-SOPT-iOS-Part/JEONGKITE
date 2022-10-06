2fd539880dc619082a8cb666ab187f3314fc2711
jeongkite <jeong6kite@gmail.com>
2022-10-07 02:37:49 +0900

    프로젝트 생성 - #1

---
+++ Assignment-1st/Assignment-1st/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment-1st
+//
+//  Created by 양정연 on 2022/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
