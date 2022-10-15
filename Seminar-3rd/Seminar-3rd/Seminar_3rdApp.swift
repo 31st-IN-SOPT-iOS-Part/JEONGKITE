cd474e680bdbe024788fc67778f1a480c7d88ac8
jeongkite <jeong6kite@gmail.com>
2022-10-15 14:30:09 +0900

    프로젝트 생성

---
+++ Seminar-3rd/Seminar-3rd/Seminar_3rdApp.swift
@@ -0,0 +1,17 @@
+//
+//  Seminar_3rdApp.swift
+//  Seminar-3rd
+//
+//  Created by 양정연 on 2022/10/15.
+//
+
+import SwiftUI
+
+@main
+struct Seminar_3rdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
