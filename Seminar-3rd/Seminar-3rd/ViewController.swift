e0ab3ae51cd7790f0c6ff987f28f6720b417a1e5
jeongkite <jeong6kite@gmail.com>
2022-10-16 17:34:37 +0900

    3차 세미나

---
+++ Seminar-3rd/Seminar-3rd/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seminar-3rd
+//
+//  Created by 양정연 on 2022/10/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
